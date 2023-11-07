63ac29414370e681cf4c6a72d11dc63dc1d3543c
PardeshDhakal <pardesh.dhakal1@gmail.com>
2023-11-07 18:43:18 -0500

    figured out connection to mongoDB

--- server/src/server.js
+++ server/src/server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -12,18 +13,31 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // Connect to MongoDB
-const MONGODB_URI = "<your_mongodb_uri>";
+
+const user = process.env.MONGODB_USER;
+const pass = process.env.MONGODB_PASS;
+const uri = process.env.MONGODB_ALT2_URI;
+let MONGODB_URI = `mongodb+srv://${user}:${pass}@${uri}`;
+
+MONGODB_URI =
+  "mongodb+srv://pard3sh:hGKecIRTJxsdvaqn@volunteerportal.zv9kfyo.mongodb.net/database0";
+
+// figure out how to do the mongoDB connectio nheheh
+// mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');
+
+const User = mongoose.model("User", { email: String, password: String });
+
+const example = User({ email: "user1@example.com", password: "abc123" });
+console.log("Pre Connected to MongoDB");
 mongoose
-  .connect(MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(MONGODB_URI)
   .then(() => {
     console.log("Connected to MongoDB");
   })
   .catch((error) => {
     console.error("MongoDB connection error", error);
   });
+console.log("Post Connected to MongoDB");
 
 // Routes
 const authRoutes = require("./routes/auth");
