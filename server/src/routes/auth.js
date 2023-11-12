15934f39f8fd7d176a2d7d0aaf1a92683a20960d
PardeshDhakal <pardesh.dhakal1@gmail.com>
2023-11-12 13:52:20 -0500

    added register route for testing

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -3,6 +3,30 @@ const router = express.Router();
 const User = require("../models/user");
 const bcrypt = require("bcrypt");
 
+router.post("/register", async (req, res) => {
+  const { email, password, role } = req.body;
+
+  try {
+    // Check if the user already exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const newUser = new User({
+      email,
+      password,
+      role,
+    });
+
+    await newUser.save();
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (error) {
+    console.error("Error during registration:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
 router.post("/login", async (req, res) => {
   const { email, password } = req.body;
   console.log("Received request with email:", email, "and password:", password);
